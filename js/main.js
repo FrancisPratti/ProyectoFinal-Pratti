c717befc075b8fa9e95558f31bf8fb90773c8f68
FrancisPratti <francopratti@hotmail.com>
2024-01-17 17:30:51 -0300

    Pruebas

--- js/main.js
+++ js/main.js
@@ -1,4 +1,105 @@
-const carasPermitidas = [0,4, 6, 8, 10, 12, 20];
+const arrayDados = [
+  {
+    id: 4,
+    titulo: "Dado de 4 caras",
+    imagen: "./img/d4.png",
+    input: "d4",
+  },
+  {
+    id: 6,
+    titulo: "Dado de 6 caras",
+    imagen: "./img/d6.png",
+    input: "d6",
+  },
+  {
+    id: 8,
+    titulo: "Dado de 8 caras",
+    imagen: "./img/d8.png",
+    input: "d8",
+  },
+  {
+    id: 10,
+    titulo: "Dado de 10 caras",
+    imagen: "./img/d10.png",
+    input: "d10",
+  },
+  {
+    id: 12,
+    titulo: "Dado de 12 caras",
+    imagen: "./img/d12.png",
+    input: "d12",
+  },
+  {
+    id: 20,
+    titulo: "Dado de 20 caras",
+    imagen: "./img/d20.png",
+    input: "d20",
+  }
+];
+
+// Definición de variables globales
+// Definición de variables globales
+let cantidadDados;
+let carasDado;
+
+// Creacion de tarjetas de Dados
+const contenedorDados = document.querySelector("#contenedor-dados")
+
+function tiposDeDados() {
+    arrayDados.forEach(dado => {
+      const div = document.createElement("div")
+      div.classList.add("dado");
+      div.innerHTML = `
+        <img src="${dado.imagen}" class="card-img-top img-dado" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${dado.titulo}</h5>
+          <h6>Cantidad de dados</h6>
+          <input type="number" min="1" max="10" class="cantidad" id="${dado.input}">
+          <button type="button" class="btn btn-primary" id="liveToastBtn${dado.id}">Tirar dados</button>
+        </div>
+      `;
+      contenedorDados.appendChild(div);
+
+      const botonRoll = contenedorDados.querySelectorAll(".btn")
+
+      botonRoll.forEach(boton => {
+        boton.addEventListener("click", () => {
+          const dadoSeleccionado = arrayDados.find(dado => dado.id === parseInt(boton.id.replace("liveToastBtn", "")));
+          cantidadDados = document.getElementById(dadoSeleccionado.input.value);
+          carasDado = parseInt(boton.id.replace("liveToastBtn", ""));
+          // Llamada a la función tirarDados solo una vez
+          const resultados = SimuladorDados.tirarDados(cantidadDados, carasDado);
+          const suma = SimuladorDados.obtenerSuma();
+          const resultadosTexto = `Resultados: ${resultados.join(', ')}\nSuma total: ${suma}`;
+          console.log(resultadosTexto);
+
+                // Mostrar los resultados en el Toast de Bootstrap
+                const resultadosShow = document.getElementById("resultados");
+                resultadosShow.innerHTML = `
+                    <div class="toast-container position-fixed bottom-0 end-0 p-3">
+                        <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+                            <div class="toast-header">
+                                <img src="..." class="rounded me-2" alt="...">
+                                <strong class="me-auto">Tu resultado:</strong>
+                                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                            </div>
+                            <div class="toast-body">
+                                ${resultados.join(', ')}<br>Suma total: ${suma}
+                            </div>
+                        </div>
+                    </div>
+                `;
+            })
+        });
+    });
+}
+
+// Llamada a la función
+tiposDeDados();
+
+
+
+//Fin creacion tarjetas de Dados
 
 const SimuladorDados = {
   resultados: [],
@@ -30,34 +131,12 @@ const SimuladorDados = {
   }
 };
 
-function obtenerCarasValidas() {
-  let caras = Number (prompt('Ingrese la cantidad de caras del dado (4, 6, 8, 10, 12 o 20) (o 0 para finalizar):', '6'));
+
+let resultados = SimuladorDados.tirarDados(cantidadDados, carasDado);
+let suma = SimuladorDados.obtenerSuma();
+let resultadosTexto = `Resultados: ${resultados.join(', ')}\nSuma total: ${suma}`;
+console.log(resultadosTexto);
   
-  while (!carasPermitidas.includes(caras)) {
-    caras = Number(prompt('Ingrese una cantidad válida de caras (4, 6, 8, 10, 12 o 20) (o 0 para finalizar):', '6'));
-  }
 
-  return caras;
-}
-
-let carasElegidas = obtenerCarasValidas();
-
-
-
-while (carasElegidas != 0) {
-  
-  let numDados = parseInt(prompt('Ingrese el número de dados a tirar:', '1'));
-
-  if (isNaN(numDados) || numDados <= 0) {
-    alert('Ingrese un número válido mayor que cero.');
-  } else {
-    let resultados = SimuladorDados.tirarDados(numDados, carasElegidas);
-    let suma = SimuladorDados.obtenerSuma();
-    let resultadosTexto = `Resultados: ${resultados.join(', ')}\nSuma total: ${suma}`;
-    alert(resultadosTexto);
-  }
-
-  carasElegidas = obtenerCarasValidas();
-}
 
 
